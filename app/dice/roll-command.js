75de38074e884d9f9a2d99f96de77e7881159d38
Charles <charles.oder@ehi.com>
2020-12-26 07:19:32 -0600

    added help messsage

--- app/dice/roll-command.js
+++ app/dice/roll-command.js
@@ -2,8 +2,20 @@ const diceRoller = require("./dice-roller.js");
 
 const prefix = "!roll";
 
+function createHelpMessage() {
+  let msg = "Usage: !roll xdx [+xdx] [+x]\n";
+  msg += "  Examples: !roll 3d6";
+  msg += "            !roll 1d20 + 5";
+  msg += "            !roll 1d8 + 2d6 + 5";
+  return msg;
+}
+
 function processMessage(message) {
   if (!message.content.startsWith(prefix)) return false;
+  if (message.content.includes("help")) {
+    message.reply(createHelpMessage());
+    return false;
+  }
   const author = message.author.username;
   const body = message.content.replace(prefix, "");
   console.log("body: " + body);
