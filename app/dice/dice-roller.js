5c9e9203b1ea87ef00f3625462d6697377ac7545
Charles <charles.oder@ehi.com>
2020-12-25 18:22:50 -0600

    added parsing for dice string

--- app/dice/dice-roller.js
+++ app/dice/dice-roller.js
@@ -1,5 +1,38 @@
-const dieRoller = {
-  
-};
+function tokenize(dieString) {
+  const trimmed = dieString.replace(/ /g, "");
+  const components = trimmed.replace(/-/g, "+-").split("+");
 
-export default dieRoller;
+  return components;
+  // xdx // normal die
+  // xdx + xdx // added dice
+  // xdx + n // dice plus modifier
+  // xdxhx // dice keeping highest x
+  // xdxlx // dice keeping lowest x
+  // -<anything> // subtract value
+}
+function createDice(token) {
+  let keep = "all";
+  let diceString = token;
+
+  const keepHigh = token.split("h");
+  if (keepHigh[1]) {
+    keep = "high-" + keepHigh[1];
+    diceString = keepHigh[0];
+  }
+
+  const keepLow = token.split("l");
+  if (keepLow[1]) {
+    keep = "low-" + keepLow[1];
+    diceString = keepLow[0];
+  }
+
+  const components = diceString.split("d");
+  const count = components[0];
+  const sides = components[1];
+  const dice = Array(parseInt(count)).fill("d" + sides);
+  
+  return { dice: dice, keep: keep };
+}
+
+module.exports.tokenize = tokenize;
+module.exports.createDice = createDice;
---
+++ test/dice-roller-test.js
@@ -0,0 +1,73 @@
+var assert = require('assert');
+var diceRoller = require("../app/dice/dice-roller.js");
+
+describe("DiceRoller", function() {
+  describe("tokenize", function() {
+    it("1d10 returns single 1d10", function() {
+      const diceString = "1d10";
+      const tokens = diceRoller.tokenize(diceString);
+
+      assert.deepStrictEqual(tokens, ["1d10"]);
+    });
+    it("1d10+2d6 returns 2 tokens", function() {
+      const diceString = "1d10+2d6";
+      const tokens = diceRoller.tokenize(diceString);
+
+      assert.deepStrictEqual(tokens, ["1d10", "2d6"]);
+    });
+    it("1d10 + 2d6 returns 2 tokens", function() {
+      const diceString = "1d10 + 2d6";
+      const tokens = diceRoller.tokenize(diceString);
+
+      assert.deepStrictEqual(tokens, ["1d10", "2d6"]);
+    });
+    it("1d10 + 2d6 + 2 returns 3 tokens", function() {
+      const diceString = "1d10 + 2d6 + 2";
+      const tokens = diceRoller.tokenize(diceString);
+
+      assert.deepStrictEqual(tokens, ["1d10", "2d6", "2"]);
+    });
+    it("1d10 + 2d6 - 2 returns 3 tokens with a negative mod", function() {
+      const diceString = "1d10 + 2d6 - 2";
+      const tokens = diceRoller.tokenize(diceString);
+
+      assert.deepStrictEqual(tokens, ["1d10", "2d6", "-2"]);
+    });
+    it("1d10 - 1d6 subtracts dice", function() {
+      const diceString = "1d10 - 1d6";
+      const tokens = diceRoller.tokenize(diceString);
+
+      assert.deepStrictEqual(tokens, ["1d10", "-1d6"]);
+    });
+  });
+  describe("createDice", function() {
+    it("1d10 returns single d10", function() {
+      const token = "1d10";
+      const dice = diceRoller.createDice(token);
+
+      assert.deepStrictEqual(["d10"], dice.dice);
+      assert.deepStrictEqual("all", dice.keep);
+    });
+    it("2d10 returns two d10s", function() {
+      const token = "2d10";
+      const dice = diceRoller.createDice(token);
+
+      assert.deepStrictEqual(["d10", "d10"], dice.dice);
+      assert.deepStrictEqual("all", dice.keep);
+    });
+    it("5d6h3 returns two 5 dice with high-3 for keep", function() {
+      const token = "5d6h3";
+      const dice = diceRoller.createDice(token);
+
+      assert.deepStrictEqual(["d6", "d6", "d6", "d6", "d6"], dice.dice);
+      assert.deepStrictEqual("high-3", dice.keep);
+    });
+    it("5d6l3 returns two 5 dice with low-3 for keep", function() {
+      const token = "5d6l3";
+      const dice = diceRoller.createDice(token);
+
+      assert.deepStrictEqual(["d6", "d6", "d6", "d6", "d6"], dice.dice);
+      assert.deepStrictEqual("low-3", dice.keep);
+    });
+  });
+});
